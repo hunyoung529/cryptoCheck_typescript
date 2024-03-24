4422f38f70ae1ee98531e18c59b9a509411df2b3
hunyoung529 <133857185+hunyoung529@users.noreply.github.com>
2024-03-24 09:43:53 +0900

    d

--- src/api.ts
+++ src/api.ts
@@ -1,30 +1,23 @@
-const BASE_URL = `https://proxy.cors.sh/https://api.coinpaprika.com/v1`;
-const API_KEY = "temp_42b93062c7f214fc68ed555daa6d0487";
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
 export function fetchCoins() {
-  return fetch(`${BASE_URL}/coins`, {
-    headers: {
-      "x-cors-api-key": API_KEY,
-    },
-  }).then((response) => response.json());
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
 
 export function fetchCoinInfo(coinId: string) {
-  return fetch(`${BASE_URL}/coins/${coinId}`, {
-    headers: {
-      "x-cors-api-key": API_KEY,
-    },
-  }).then((response) => response.json());
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
+    response.json()
+  );
 }
 
 export function fetchCoinTickers(coinId: string) {
-  return fetch(`${BASE_URL}/tickers/${coinId}`, {
-    headers: {
-      "x-cors-api-key": API_KEY,
-    },
-  }).then((response) => response.json());
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
 }
 
-export function fetchCoinHistory(coinId:string){
-  return fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)
-  .then((response) => response.json());  
+export function fetchCoinHistory(coinId: string) {
+  return fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+  ).then((response) => response.json());
 }
