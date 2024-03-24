9a762c4c3036b9ef376102da47239675331e2081
hunyoung529 <133857185+hunyoung529@users.noreply.github.com>
2024-03-24 09:53:59 +0900

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import Chart from "./routes/Chart";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Coins />} />
         <Route path="/:coinId" element={<Coin />}>
